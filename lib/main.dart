af2d78981d7e2c3f160b942002abc6b938ddb414
luis_mantilla <lmmantilla9350@gmail.com>
2019-03-23 12:35:44 -0400

    Add card room

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,73 @@ void main(){
             textDirection: TextDirection.ltr,
           ),
         ),
-        body: Center(
-          child: Text(
-            "Hello!!!",
-            textDirection: TextDirection.ltr,
-          ),
-        ),
+        body: CardRoom()
       ),
 
     )
   );
+}
+
+class CardRoom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final cardTitle = Text(
+      "Hello!!!",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+          color: Colors.white,
+          fontSize: 24.0,
+          fontWeight: FontWeight.w500
+      ),
+    );
+
+    final cardArrowFab = Positioned(
+      bottom: 10.0,
+      right: 10.0,
+      child: FloatingActionButton(
+        backgroundColor: Colors.black38,
+        child: Icon(
+          Icons.arrow_forward_ios,
+          color: Colors.white,
+        ),
+      ),
+    );
+
+    final cardLightFab = Positioned(
+      top: 10.0,
+      right: 10.0,
+      child: FloatingActionButton(
+        backgroundColor: Colors.transparent,
+        child: Icon(
+          Icons.lightbulb_outline,
+          color: Colors.white,
+        ),
+      ),
+    );
+
+    final cardContainer = Container(
+      height: 200.0,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/pic_house.jpg'),
+          fit: BoxFit.cover
+        ),
+        borderRadius: BorderRadius.circular(20.0)
+      )
+    );
+
+
+    return Stack(
+      children: <Widget>[
+        cardContainer,
+        Positioned(
+          bottom: 10.0,
+          left: 10.0,
+          child: cardTitle,
+        ),
+        cardArrowFab,
+        cardLightFab
+      ],
+    );
+  }
 }
