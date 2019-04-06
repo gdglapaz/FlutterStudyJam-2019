b397add1dac8863b4efa66f2d6630e6e67582dbd
luis_mantilla <lmmantilla9350@gmail.com>
2019-04-06 11:58:01 -0400

    Finish Custom Card

--- anatomic_app/lib/screens/home_screen.dart
+++ anatomic_app/lib/screens/home_screen.dart
@@ -3,16 +3,31 @@ import 'package:anatomic_app/widgets/custom_text.dart';
 import 'package:anatomic_app/widgets/custom_card.dart';
 
 class HomeScreen extends StatelessWidget {
+  Color mPurpleColor = Color(0xFF6200ea);
+
   @override
   Widget build(BuildContext context) {
+    final mVerticalList = Container(
+      height: 240.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          CustomCard.vertical(2, "Prueba 2", "assets/pic_1.jpg",Icon(Icons.account_balance_wallet), mPurpleColor ),
+          CustomCard.vertical(3, "Prueba 3", "assets/pic_2.jpg",Icon(Icons.airline_seat_recline_extra), Color(0xFF0091ea) ),
+          CustomCard.vertical(4, "Prueba 4", "assets/pic_3.jpg",Icon(Icons.accessible_forward),Color(0xFF00bfa5)),
+        ],
+      ),
+    );
+
     return Container(
       color: Colors.white,
       child: ListView(
         children: <Widget>[
           CustomText("April 6th", Colors.grey, 18.0),
           CustomText("Ready to Learn?", Colors.deepPurple, 24.0),
-          CustomCard(1, "Prueba", Icon(Icons.account_balance_wallet), ),
+          CustomCard(1, "Prueba", "assets/bones_1.jpg",Icon(Icons.account_balance_wallet), ),
           CustomText("Handpicked", Colors.deepPurple, 20.0),
+          mVerticalList
         ],
       ),
     );
--- anatomic_app/lib/widgets/custom_card.dart
+++ anatomic_app/lib/widgets/custom_card.dart
@@ -3,19 +3,35 @@ import 'package:anatomic_app/widgets/custom_text.dart';
 
 class CustomCard extends StatelessWidget {
   double mPaddingCard = 10.0;
+  double mCustomWidth = 300;
+  double mCustomHeight = 200;
+
   int mId;
   String mTitle;
+  String mPathImage;
   Icon mIcon;
+  Color mFilter = Colors.deepPurple;
+  double mOpacity = 0.3;
 
-  CustomCard(this.mId, this.mTitle, this.mIcon);
+  CustomCard(this.mId, this.mTitle, this.mPathImage,this.mIcon);
+  CustomCard.vertical(this.mId, this.mTitle, this.mPathImage,this.mIcon, this.mFilter){
+    this.mOpacity = 0.1;
+    this.mCustomWidth = 170.0;
+    this.mCustomHeight = 230.0;
+  }
 
   @override
   Widget build(BuildContext context) {
 
     final mContainer = Container(
       decoration: BoxDecoration(
+        color: mFilter,
         borderRadius: BorderRadius.circular(15.0),
-        color: Colors.deepPurple,
+        image: DecorationImage(
+            colorFilter: ColorFilter.mode(mFilter.withOpacity(mOpacity), BlendMode.dstATop),
+            image: AssetImage(mPathImage),
+            fit: BoxFit.cover
+        )
       ),
     );
 
@@ -42,8 +58,9 @@ class CustomCard extends StatelessWidget {
     );
 
     return Container(
-      width: 300.0,
-      height: 200.0,
+      width: mCustomWidth,
+      height: mCustomHeight,
+      margin: EdgeInsets.only(left: 10.0, right: 10.0),
       child: Stack(
         children: <Widget>[
           mContainer,
