0065b81a4603811437a33ecd9863ed721c1b4a92
luis_mantilla <lmmantilla9350@gmail.com>
2019-04-06 11:21:19 -0400

    Add Custom Card Widget

--- anatomic_app/lib/screens/home_screen.dart
+++ anatomic_app/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:anatomic_app/widgets/custom_text.dart';
+import 'package:anatomic_app/widgets/custom_card.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -10,6 +11,7 @@ class HomeScreen extends StatelessWidget {
         children: <Widget>[
           CustomText("April 6th", Colors.grey, 18.0),
           CustomText("Ready to Learn?", Colors.deepPurple, 24.0),
+          CustomCard(1, "Prueba", Icon(Icons.account_balance_wallet), ),
           CustomText("Handpicked", Colors.deepPurple, 20.0),
         ],
       ),
---
+++ anatomic_app/lib/widgets/custom_card.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:anatomic_app/widgets/custom_text.dart';
+
+class CustomCard extends StatelessWidget {
+  double mPaddingCard = 10.0;
+  int mId;
+  String mTitle;
+  Icon mIcon;
+
+  CustomCard(this.mId, this.mTitle, this.mIcon);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final mContainer = Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+        color: Colors.deepPurple,
+      ),
+    );
+
+    final mIndicators = Row(
+      children: <Widget>[
+        Icon(
+          Icons.access_time,
+          color: Colors.white,
+        ),
+        CustomText("15'", Colors.white, 16.0),
+        Icon(
+          Icons.timelapse,
+          color: Colors.white,
+        ),
+        CustomText("6", Colors.white, 16.0)
+      ],
+    );
+
+    final mFab = FloatingActionButton(
+      backgroundColor: Colors.black26,
+      child: mIcon,
+    );
+
+    return Container(
+      width: 300.0,
+      height: 200.0,
+      child: Stack(
+        children: <Widget>[
+          mContainer,
+          Positioned(
+            bottom: mPaddingCard,
+            left: mPaddingCard,
+            child: CustomText(mTitle, Colors.white, 22.0),
+          ),
+          Positioned(
+            top: mPaddingCard,
+            left: mPaddingCard,
+            child: mIndicators,
+          ),
+          Positioned(
+            right: mPaddingCard,
+            top: mPaddingCard,
+            child: mFab,
+          )
+        ],
+      ),
+    );
+  }
+}
